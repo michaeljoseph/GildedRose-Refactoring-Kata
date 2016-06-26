27f717e5553d22d47d00fa5227a98f608a3239d6
Michael Joseph <michaeljoseph+github@gmail.com>
2016-06-26 17:09:14 +0200

    Clarifying comments

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -11,30 +11,42 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
+            # quality decreases
             if item.name != BRIE and item.name != BACKSTAGE_PASS:
+                # quality cannot decrease beyond zero
                 if item.quality > 0:
+                    # sulfuras never changes
                     if item.name != SULFURAS:
                         item.quality = item.quality - 1
+            # quality increases
             else:
+                # quality cannot increase beyond 50
                 if item.quality < 50:
                     item.quality = item.quality + 1
-                    if item.name ==  BACKSTAGE_PASS:
+                    if item.name == BACKSTAGE_PASS:
                         if item.sell_in < 11:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
                         if item.sell_in < 6:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
+
+            # sell by always decreases (except sulfuras)
             if item.name != SULFURAS:
                 item.sell_in = item.sell_in - 1
+
+            # past sell by
             if item.sell_in < 0:
                 if item.name != BRIE:
                     if item.name != BACKSTAGE_PASS:
+                        # normal item
                         if item.quality > 0:
                             if item.name != SULFURAS:
                                 item.quality = item.quality - 1
+                    # backstage is zero past sell by
                     else:
                         item.quality = item.quality - item.quality
+                # brie quality increases past sell by
                 else:
                     if item.quality < 50:
                         item.quality = item.quality + 1
